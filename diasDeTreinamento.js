1457c7422e5bf10e4c31f96be42a0e68f91315c8
Rebeca Primo <rebecarodriguesprimo036@gmail.com>
2023-08-23 21:20:59 -0300

    Add files via upload

---
+++ diasDeTreinamento.js
@@ -0,0 +1,37 @@
+const nome = "Seu Nome";
+
+function getEventoAleatorio() {
+  const numeroAleatorio = Math.floor(Math.random() * 3); // Gera um número entre 0 e 2
+
+  if (numeroAleatorio === 0) {
+    return "Maratona";
+  } else if (numeroAleatorio === 1) {
+    return "Triathlon";
+  } else {
+    return "Pentathlon";
+  }
+}
+
+function getDiasTreino(evento) {
+  if (evento === "Maratona") {
+    return 50;
+  } else if (evento === "Triathlon") {
+    return 100;
+  } else {
+    return 200;
+  }
+}
+
+function logEvento(nome, evento) {
+  console.log(`${nome} vai participar do evento: ${evento}`);
+}
+
+function logTempo(nome, dias) {
+  console.log(`${nome} vai treinar por ${dias} dias`);
+}
+
+const eventoAleatorio = getEventoAleatorio();
+const diasTreino = getDiasTreino(eventoAleatorio);
+
+logEvento(nome, eventoAleatorio);
+logTempo(nome, diasTreino);
